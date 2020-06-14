1e77d2aea7dd402f882f9d54c472f00c92c4c4c3
Sergio Ramirez <sergioramirez1097@gmail.com>
2020-06-14 08:57:21 -0500

    Front index, formulariomy script

---
+++ js/script.js
@@ -0,0 +1,27 @@
+function addRow() {
+      document.getElementById("tabledata").insertRow(-1).innerHTML = '<tr><th scope="row">  <input class="form-control form-control-sm" type="text" value="Alt"> </th><td>  <input class="form-control form-control-sm" type="text" placeholder="Ingrese el valor"> </td><td>  <input class="form-control form-control-sm" type="text" placeholder="Ingrese el valor"> </td></tr>';
+}
+
+function deleteRow() {
+      var table = document.getElementById("tabledata");
+      var rowCount = table.rows.length;
+      //console.log(rowCount);
+
+      if (rowCount <= 1)
+            alert('No se puede eliminar el encabezado');
+      else
+            table.deleteRow(rowCount - 1);
+}
+
+function maximax(){
+      $.ajax({
+            method: "POST",
+            url: "http:/"
+          }).done(function(data) {
+            alert(data); // imprimimos la respuesta
+          }).fail(function() {
+            alert("Algo salió mal");
+          }).always(function() {
+            alert("Siempre se ejecuta")
+          });
+}
