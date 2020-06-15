85c114dca0084785ade4005e6478e6d1656e530f
Martha Mejia <mathalu18@gmail.com>
2020-06-15 10:45:26 -0500

    ajax para no probabilisticos

--- js/script.js
+++ js/script.js
@@ -7,9 +7,13 @@
             $("#aumentar-fila").on('click', addRow)
             $("#eliminar-fila").on('click', deleteRow)
             $("#maximin").on('click', maximin)
+            $("#maximax").on('click', maximax)
+            $("#regret").on('click', regret)
+            $("#hardwiz").on('click', hardwiz)
+            $("#metodos").on("change", vistasMetodos)
       })
       function addRow() {
-            let filas = '<tr><th scope="row">  <input class="form-control form-control-sm" type="text" value="Alt"> </th><td>  <input class="form-control form-control-sm" type="text" placeholder="Ingrese el valor"> </td><td>  <input class="form-control form-control-sm" type="text" placeholder="Ingrese el valor"> </td></tr>'
+            let filas = '<tr><td scope="row">  <input class="form-control form-control-sm" type="text" value="Alt"> </td><td>  <input class="form-control form-control-sm" type="text" placeholder="Ingrese el valor"> </td><td>  <input class="form-control form-control-sm" type="text" placeholder="Ingrese el valor"> </td></tr>'
             $("#tabledata").append(filas)
       }
 
@@ -32,20 +36,95 @@
                   })
                   matrizAux.push(matrizVal);
             })
-            matriz.matriz = matrizAux
+            matriz.Matriz = matrizAux
       }
 
       function maximin() {
             armarMatriz()
+            console.log(JSON.stringify(matriz));
+
             $.ajax({
-                  type: "POST", 
-                  crossDomain: true,
-                  dataType: 'json',
+                  headers: {
+                        "Content-Type": "application/json"
+                  },
+                  type: "POST",
+                  dataType: "json",
                   url: "https://invoperacionesapi.azurewebsites.net/api/Metodos/GetWaldCriterion",
-                  data: matriz, 
+                  data: JSON.stringify(matriz),
                   success: function (datos) {
                         console.log(datos)
                   },
             })
+      }
+
+      function maximax() {
+            armarMatriz()
+            console.log(JSON.stringify(matriz));
+
+            $.ajax({
+                  headers: {
+                        "Content-Type": "application/json"
+                  },
+                  type: "POST",
+                  dataType: "json",
+                  url: "https://invoperacionesapi.azurewebsites.net/api/Metodos/GetMaximaxCriterion",
+                  data: JSON.stringify(matriz),
+                  success: function (datos) {
+                        console.log(datos)
+                  },
+            })
+      }
+
+      function regret() {
+            armarMatriz()
+            console.log(matriz);
+
+            $.ajax({
+                  headers: {
+                        "Content-Type": "application/json"
+                  },
+                  type: "POST",
+                  dataType: "json",
+                  url: "https://invoperacionesapi.azurewebsites.net/api/Metodos/GetSavageCriterion",
+                  data: JSON.stringify(matriz),
+                  success: function (datos) {
+                        console.log(datos)
+                  },
+            })
+      }
+
+      function hardwiz() {
+            armarMatriz()
+            matriz.Alfa = $("#alfaVal").val()
+            $.ajax({
+                  headers: {
+                        "Content-Type": "application/json"
+                  },
+                  type: "POST",
+                  dataType: "json",
+                  url: "https://invoperacionesapi.azurewebsites.net/api/Metodos/GetHurwiczCriterion",
+                  data: JSON.stringify(matriz),
+                  success: function (datos) {
+                        console.log(datos)
+                  },
+            })
+      }
+
+      function vistasMetodos() {
+            let opciones = parseInt($(this).val())
+            switch (opciones) {
+                  case 1:
+                        $("#no-probablistic").attr('hidden', true)
+                        $("#probablistic").removeAttr('hidden')
+                        $("#alfa").attr('hidden', true)
+                        break
+                  case 2:
+                        $("#probablistic").attr('hidden', true)
+                        $("#no-probablistic").removeAttr('hidden')
+                        $("#alfa").removeAttr('hidden')
+                        break
+                  default:
+                        break
+            }
       }
 }))
