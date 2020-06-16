bffee2dde998f914b6e42e9a01ab36215ebcdb1b
Martha Mejia <mathalu18@gmail.com>
2020-06-15 19:43:58 -0500

    probabilisticos finalizado

--- js/script.js
+++ js/script.js
@@ -12,6 +12,9 @@
             $("#maximax").on('click', maximax)
             $("#regret").on('click', regret)
             $("#hardwiz").on('click', hardwiz)
+            $("#emv").on('click', emv)
+            $("#eol").on('click', eol)
+            $("#sensibility").on('click', sensibiltyAnalisis)
             $("#metodos").on("change", vistasMetodos)
       })
       function addRow() {
@@ -19,29 +22,48 @@
             $("#tabledata").append(filas)
       }
 
-      function addColumn(){
+      function addColumn() {
             var table = document.getElementById('tabledata');
             var colCount = table.rows[0].cells.length;
             var rowCount = table.rows.length;
-            for(var i=0; i<rowCount; i++){            
-                 var row = table.rows[i];
-                 var col = row.insertCell(colCount);
-                 if(i == 0){
-                  col.innerHTML = '<th><b>Evento</b></th>';
-                 }else{
-                  col.innerHTML = '<td>  <input class="form-control form-control-sm" type="text" placeholder="Ingrese el valor"> </td>';
-                 }                                  
-            } 
+            for (var i = 0; i < rowCount; i++) {
+                  var row = table.rows[i];
+                  var col = row.insertCell(colCount);
+                  if (i == 0) {
+                        col.innerHTML = '<th><b>Evento</b></th>';
+                  } else {
+                        col.innerHTML = '<td>  <input class="form-control form-control-sm" type="text" placeholder="Ingrese el valor"> </td>';
+                  }
+            }
+            var table = document.getElementById('probability');
+            var colCount = table.rows[0].cells.length;
+            var rowCount = table.rows.length;
+            for (var i = 0; i < rowCount; i++) {
+                  var row = table.rows[i];
+                  var col = row.insertCell(colCount);
+                  if (i == 0) {
+                        col.innerHTML = '<th><b>Evento</b></th>';
+                  } else {
+                        col.innerHTML = '<td>  <input class="form-control form-control-sm" type="text" placeholder="Ingrese el valor"> </td>';
+                  }
+            }
       }
 
-      function deleteColumn(){
+      function deleteColumn() {
             var table = document.getElementById('tabledata');
             var colCount = table.rows[0].cells.length;
             var rowCount = table.rows.length;
-            for(var i=0; i<rowCount; i++){            
-                 var row = table.rows[i];
-                 row.deleteCell(colCount);                                  
-            } 
+            for (var i = 0; i < rowCount; i++) {
+                  var row = table.rows[i];
+                  row.deleteCell(colCount - 1);
+            }
+            var table = document.getElementById('probability');
+            var colCount = table.rows[0].cells.length;
+            var rowCount = table.rows.length;
+            for (var i = 0; i < rowCount; i++) {
+                  var row = table.rows[i];
+                  row.deleteCell(colCount - 1);
+            }
       }
 
       function deleteRow() {
@@ -55,9 +77,10 @@
                   table.deleteRow(rowCount - 1);
       }
 
-      
+
       function armarMatriz() {
-            $("tbody tr").each(function () {
+            matrizAux = []
+            $("#tabledata tbody tr").each(function () {
                   let matrizVal = []
                   $(this).find('td').each(function () {
                         matrizVal.push($(this).find('input').val())
@@ -66,7 +89,6 @@
             })
 
             matriz.Matriz = matrizAux
-
       }
 
       function maximin() {
@@ -147,14 +169,126 @@
                         $("#no-probablistic").attr('hidden', true)
                         $("#probablistic").removeAttr('hidden')
                         $("#alfa").attr('hidden', true)
+                        $("#probability").removeAttr('hidden')
                         break
                   case 2:
                         $("#probablistic").attr('hidden', true)
                         $("#no-probablistic").removeAttr('hidden')
                         $("#alfa").removeAttr('hidden')
+                        $("#probability").attr('hidden', true)
                         break
                   default:
                         break
             }
+      }
+
+      function emv() {
+            armarMatriz()
+            armarProbabilidades()
+            console.log(JSON.stringify(matriz));
+            $.ajax({
+                  headers: {
+                        "Content-Type": "application/json"
+                  },
+                  type: "POST",
+                  dataType: "json",
+                  url: "https://invoperacionesapi.azurewebsites.net/api/Metodos/GetMatrizEMV",
+                  data: JSON.stringify(matriz),
+                  success: function (datos) {
+                        tableProbabilisticResults(datos)
+                  },
+            })
+      }
+
+      function armarProbabilidades() {
+            let matrizVal = []
+            $("#probability tbody tr td").each(function () {
+                  matrizVal.push($(this).find('input').val())
+            })
+            matrizVal.shift()
+            matriz.Probability = matrizVal
+      }
+
+      function tableProbabilisticResults(datos) {
+            let encabezado = document.createElement('thead')
+            let cuerpoTabla = document.createElement('tbody')
+            let titulos = document.createElement('tr')
+            for (let index = 0; index < datos.Matriz[0]; index++) {
+                  if (dato == "null") {
+                        dato = "Alternativas"
+                  }
+                  let titulo = document.createElement('th')
+                  titulos.appendChild(document.createTextNode(dato));
+                  titulo.appendChild(celda)
+                  encabezado.appendChild(titulo)
+            }
+
+            datos.Matriz.forEach(function (datosFilas) {
+                  let fila = document.createElement('tr')
+                  datosFilas.forEach(function (datosCeldas) {
+                        let celda = document.createElement('td')
+                        celda.appendChild(document.createTextNode(datosCeldas))
+                        fila.appendChild(celda)
+                  });
+
+                  cuerpoTabla.appendChild(fila)
+            });
+
+            $("#results").html(encabezado)
+            $("#results").html(cuerpoTabla)
+            $("#resultados").html($("#results"))
+      }
+
+      function eol() {
+            armarMatriz()
+            armarProbabilidades()
+            console.log(JSON.stringify(matriz));
+            $.ajax({
+                  headers: {
+                        "Content-Type": "application/json"
+                  },
+                  type: "POST",
+                  dataType: "json",
+                  url: "https://invoperacionesapi.azurewebsites.net/api/Metodos/GetMatrizEOL",
+                  data: JSON.stringify(matriz),
+                  success: function (datos) {
+                        tableProbabilisticResults(datos)
+                  },
+            })
+      }
+
+      function sensibiltyAnalisis() {
+            armarMatriz()
+            armarProbabilidades()
+            console.log(JSON.stringify(matriz));
+            $.ajax({
+                  headers: {
+                        "Content-Type": "application/json"
+                  },
+                  type: "POST",
+                  dataType: "json",
+                  url: "https://invoperacionesapi.azurewebsites.net/api/Metodos/GetSensitivityAnalysis",
+                  data: JSON.stringify(matriz),
+                  success: function (datos) {
+                        armarGrafico(datos)
+                  },
+            })
+      }
+
+      function armarGrafico(datos) {
+            let alternativas = []
+            let columnas = datos.Matriz.length - 1
+            for (let i = 1; i <= columnas; i++) {
+                  let objetosAlternativas = {}
+                  objetosAlternativas.x = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
+                  objetosAlternativas.y = datos.Matriz[i].splice(1, 12)
+                  objetosAlternativas.mode = 'lines+markers'
+                  objetosAlternativas.type = 'scatter'
+                  objetosAlternativas.name = datos.Matriz[i][0]
+                  alternativas.push(objetosAlternativas)
+            }
+            var data = alternativas
+            Plotly.newPlot('grafico', data);
+
       }
 }))
